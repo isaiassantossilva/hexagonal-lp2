6643d2daed953f8992022ef88243da96a0a87705
isaiassantossilva <isaias661248@gmail.com>
2021-06-17 16:55:50 -0300

    first commit

---
+++ smtp-mail/src/main/java/com/codedevs/smtpmail/config/JavaMailSenderConfig.java
@@ -0,0 +1,31 @@
+package com.codedevs.smtpmail.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class JavaMailSenderConfig {
+
+    @Bean
+    public JavaMailSender getJavaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+
+        mailSender.setUsername("isaias661248@gmail.com");
+        mailSender.setPassword("amoadeus123456");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+
+}
